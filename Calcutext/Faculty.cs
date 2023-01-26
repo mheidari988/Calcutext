3c9bb9526e18998fff60a34e3336e1d57bb73463
Reza Heidari <reza.heidari@amplicade.com>
2023-01-26 22:23:22 +0300

    add Faculty class

---
+++ Calcutext/Faculty.cs
@@ -0,0 +1,37 @@
+﻿namespace Calcutext;
+public class Faculty : Operation
+{
+    public Faculty(double Value)
+    {
+        LeftValue = Value;
+        Printable = () => $"({Value}";
+        PrintableSentence = () => $"Faculty of {Value}";
+    }
+
+    public Faculty(IOperation Value)
+    {
+        LeftValue = Value.ToResult();
+        Printable = () => $"({Value.Printable.Invoke()}";
+        PrintableSentence = () => $"Faculty of {Value.PrintableSentence.Invoke()}";
+    }
+
+    public override double ToResult()
+    {
+        double result = 1;
+        for (int i = 1; i <= LeftValue; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    public override string Print()
+    {
+        return $"{Printable.Invoke()}!) = " + ToResult();
+    }
+
+    public override string PrintSentence()
+    {
+        return PrintableSentence.Invoke() + " is " + ToResult();
+    }
+}
