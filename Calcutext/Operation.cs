7d0e748c5d83d1056429a7b401171ac6009b33f4
Reza Heidari <reza.heidari@amplicade.com>
2023-01-26 21:46:32 +0300

    add base classes
        + IOperation as a contract to hold main operation
        + Operation to abstract and implement top level functionalities

---
+++ Calcutext/Operation.cs
@@ -0,0 +1,11 @@
+﻿namespace Calcutext;
+public abstract class Operation : IOperation
+{
+    public Func<string> Printable { get; set; } = () => string.Empty;
+    public Func<string> PrintableSentence { get; set; } = () => string.Empty;
+    protected double LeftValue { get; set; }
+    protected double RightValue { get; set; }
+    public abstract double ToResult();
+    public abstract string Print();
+    public abstract string PrintSentence();
+}
