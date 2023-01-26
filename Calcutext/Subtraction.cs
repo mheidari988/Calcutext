61dd56d50fa3cf7c6337b590b521a4d1b20aecb4
Reza Heidari <reza.heidari@amplicade.com>
2023-01-26 22:36:13 +0300

    add Subtraction class

---
+++ Calcutext/Subtraction.cs
@@ -0,0 +1,49 @@
+﻿namespace Calcutext;
+public class Subtraction : Operation
+{
+    public Subtraction(double Left, double Right)
+    {
+        LeftValue = Left;
+        RightValue = Right;
+        Printable = () => $"({Left} - {Right})";
+        PrintableSentence = () => $"Subtraction of {Left} and {Right}";
+    }
+
+    public Subtraction(double Left, IOperation Right)
+    {
+        LeftValue = Left;
+        RightValue = Right.ToResult();
+        Printable = () => $"({Left} - {Right.Printable.Invoke()})";
+        PrintableSentence = () => $"Subtraction of {Left} and {Right.PrintableSentence.Invoke()}";
+    }
+
+    public Subtraction(IOperation Left, double Right)
+    {
+        LeftValue = Left.ToResult();
+        RightValue = Right;
+        Printable = () => $"({Left.Printable.Invoke()} - {Right})";
+        PrintableSentence = () => $"Subtraction of {Left.PrintableSentence.Invoke()} and {Right}";
+    }
+
+    public Subtraction(IOperation Left, IOperation Right)
+    {
+        LeftValue = Left.ToResult();
+        RightValue = Right.ToResult();
+        Printable = () => $"({Left.Printable.Invoke()} - {Right.Printable.Invoke()})";
+        PrintableSentence = () => $"Subtraction of {Left.PrintableSentence.Invoke()} and {Right.PrintableSentence.Invoke()}";
+    }
+
+    public override double ToResult()
+    {
+        return LeftValue - RightValue;
+    }
+    public override string Print()
+    {
+        return $"{Printable.Invoke()} = {ToResult()}";
+    }
+
+    public override string PrintSentence()
+    {
+        return PrintableSentence.Invoke() + " is " + ToResult();
+    }
+}
