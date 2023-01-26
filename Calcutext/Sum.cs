eeb0d097e125c17692e848318fce06957328cb83
Reza Heidari <reza.heidari@amplicade.com>
2023-01-26 21:48:20 +0300

    add Sum class

---
+++ Calcutext/Sum.cs
@@ -0,0 +1,49 @@
+﻿namespace Calcutext;
+public class Sum : Operation
+{
+    public Sum(double Left, double Right)
+    {
+        LeftValue = Left;
+        RightValue = Right;
+        Printable = () => $"({Left} + {Right})";
+        PrintableSentence = () => $"Sum of {Left} and {Right}";
+    }
+
+    public Sum(double Left, IOperation Right)
+    {
+        LeftValue = Left;
+        RightValue = Right.ToResult();
+        Printable = () => $"({Left} + {Right.Printable.Invoke()})";
+        PrintableSentence = () => $"Sum of {Left} and {Right.PrintableSentence.Invoke()}";
+    }
+
+    public Sum(IOperation Left, double Right)
+    {
+        LeftValue = Left.ToResult();
+        RightValue = Right;
+        Printable = () => $"({Left.Printable.Invoke()} + {Right})";
+        PrintableSentence = () => $"Sum of {Left.PrintableSentence.Invoke()} and {Right}";
+    }
+
+    public Sum(IOperation Left, IOperation Right)
+    {
+        LeftValue = Left.ToResult();
+        RightValue = Right.ToResult();
+        Printable = () => $"({Left.Printable.Invoke()} + {Right.Printable.Invoke()})";
+        PrintableSentence = () => $"Sum of {Left.PrintableSentence.Invoke()} and {Right.PrintableSentence.Invoke()}";
+    }
+
+    public override double ToResult()
+    {
+        return LeftValue + RightValue;
+    }
+    public override string Print()
+    {
+        return $"{Printable.Invoke()} = {ToResult()}";
+    }
+
+    public override string PrintSentence()
+    {
+        return PrintableSentence.Invoke() + " is " + ToResult();
+    }
+}
