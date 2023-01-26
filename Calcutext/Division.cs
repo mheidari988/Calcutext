2e3f73b4b3bc4cfdbb3d7a2ae7e803dc1d80f323
Reza Heidari <reza.heidari@amplicade.com>
2023-01-26 22:15:56 +0300

    add Division class

---
+++ Calcutext/Division.cs
@@ -0,0 +1,49 @@
+﻿namespace Calcutext;
+public class Division : Operation
+{
+    public Division(double Left, double Right)
+    {
+        LeftValue = Left;
+        RightValue = Right;
+        Printable = () => $"({Left} / {Right})";
+        PrintableSentence = () => $"Division of {Left} by {Right}";
+    }
+
+    public Division(double Left, IOperation Right)
+    {
+        LeftValue = Left;
+        RightValue = Right.ToResult();
+        Printable = () => $"({Left} / {Right.Printable.Invoke()})";
+        PrintableSentence = () => $"Division of {Left} by {Right.PrintableSentence.Invoke()}";
+    }
+
+    public Division(IOperation Left, double Right)
+    {
+        LeftValue = Left.ToResult();
+        RightValue = Right;
+        Printable = () => $"({Left.Printable.Invoke()} / {Right})";
+        PrintableSentence = () => $"Division of {Left.PrintableSentence.Invoke()} by {Right}";
+    }
+
+    public Division(IOperation Left, IOperation Right)
+    {
+        LeftValue = Left.ToResult();
+        RightValue = Right.ToResult();
+        Printable = () => $"({Left.Printable.Invoke()} / {Right.Printable.Invoke()})";
+        PrintableSentence = () => $"Division of {Left.PrintableSentence.Invoke()} by {Right.PrintableSentence.Invoke()}";
+    }
+
+    public override double ToResult()
+    {
+        return LeftValue / RightValue;
+    }
+    public override string Print()
+    {
+        return $"{Printable.Invoke()} = {ToResult()}";
+    }
+
+    public override string PrintSentence()
+    {
+        return PrintableSentence.Invoke() + " is " + ToResult();
+    }
+}
