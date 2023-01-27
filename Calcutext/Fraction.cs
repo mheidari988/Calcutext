d20d87c93640c747765587d613a4be7bf46b9f4e
Reza Heidari <reza.heidari@amplicade.com>
2023-01-27 10:37:06 +0300

    refactor Fraction's hardcoded strings

--- Calcutext/Fraction.cs
+++ Calcutext/Fraction.cs
@@ -1,35 +1,37 @@
 ﻿namespace Calcutext;
 public class Fraction : Operation
 {
+    public const string OperationSymbol = "/";
+    
     public Fraction(double Numerator, double Denominator)
     {
         LeftValue = Numerator;
         RightValue = Denominator;
-        Printable = () => $"({Numerator}/{Denominator})";
-        PrintableSentence = () => $"{Numerator}/{Denominator}";
+        Printable = () => $"({Numerator}{OperationSymbol}{Denominator})";
+        PrintableSentence = () => $"{Numerator}{OperationSymbol}{Denominator}";
     }
     public Fraction(double Numerator, IOperation Denominator)
     {
         LeftValue = Numerator;
         RightValue = Denominator.ToResult();
-        Printable = () => $"({Numerator}/{Denominator.Printable.Invoke()})";
-        PrintableSentence = () => $"{Numerator}/{Denominator.PrintableSentence.Invoke()}";
+        Printable = () => $"({Numerator}{OperationSymbol}{Denominator.Printable.Invoke()})";
+        PrintableSentence = () => $"{Numerator}{OperationSymbol}{Denominator.PrintableSentence.Invoke()}";
     }
 
     public Fraction(IOperation Numerator, double Denominator)
     {
         LeftValue = Numerator.ToResult();
         RightValue = Denominator;
-        Printable = () => $"({Numerator.Printable.Invoke()}/{Denominator})";
-        PrintableSentence = () => $"{Numerator.PrintableSentence.Invoke()}/{Denominator}";
+        Printable = () => $"({Numerator.Printable.Invoke()}{OperationSymbol}{Denominator})";
+        PrintableSentence = () => $"{Numerator.PrintableSentence.Invoke()}{OperationSymbol}{Denominator}";
     }
 
     public Fraction(IOperation Numerator, IOperation Denominator)
     {
         LeftValue = Numerator.ToResult();
         RightValue = Denominator.ToResult();
-        Printable = () => $"({Numerator.Printable.Invoke()}/{Denominator.Printable.Invoke()})";
-        PrintableSentence = () => $"{Numerator.PrintableSentence.Invoke()}/{Denominator.PrintableSentence.Invoke()}";
+        Printable = () => $"({Numerator.Printable.Invoke()}{OperationSymbol}{Denominator.Printable.Invoke()})";
+        PrintableSentence = () => $"{Numerator.PrintableSentence.Invoke()}{OperationSymbol}{Denominator.PrintableSentence.Invoke()}";
     }
 
     public override double ToResult() => LeftValue / RightValue;
